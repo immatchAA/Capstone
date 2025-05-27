359d4b6b16d7b7503152d87253303f3b0de2f1f5
Vargas, Kim B. <kim.vargas@cit.edu>
2025-05-27 18:55:52 +0800

    Cleaned up some useless components

--- ARchiquest Mobile (Students)/ARchiQuestStudents/App.js
+++ ARchiquest Mobile (Students)/ARchiQuestStudents/App.js
@@ -3,22 +3,13 @@ import UserRegister from './Authentication/UserRegister';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './WelcomeScreen';
-import Dashboard from './Dashboard/Dashboard';
-import Progress from './StudentProgressMonitoring/Progress';
-import InsightPanel from './StudentProgressMonitoring/InsightPanel';
-import StudentProgress from './StudentProgressMonitoring/StudentProgress';
-import DesignPlan from './UploadDesign/DesignPlan';
-import MaterialsTab from './UploadDesign/MaterialsTab';
 import MainLanding from './Dashboard/mainLanding';
 import ReadingMaterials from './ReadingMaterials/ReadingMaterials';
 import ReadingMaterialDetails from './ReadingMaterials/ReadingMaterialDetails';
 import Profile from './Profile/Profile';
-
 import DesignPlanDetails from './DesignPlan/DesignPlanDetails'; 
 import DesignProgressScreen from './DesignPlan/DesignProgressScreen';
 import DesignPlanViewer from './DesignPlan/DesignPlanViewer';
-
-//Student Scoring System
 
 
 const Stack = createStackNavigator();
@@ -30,12 +21,6 @@ export default function App() {
         <Stack.Screen name='WelcomeScreen' component={WelcomeScreen}/>
         <Stack.Screen name='UserLogin' component={UserLogin}/>
         <Stack.Screen name='UserRegister' component={UserRegister}/>
-        <Stack.Screen name='Dashboard' component={Dashboard} /> 
-        <Stack.Screen name='Progress' component={Progress} />
-        <Stack.Screen name='InsightPanel' component={InsightPanel} />
-        <Stack.Screen name='StudentProgress' component={StudentProgress} />
-        <Stack.Screen name='DesignPlan' component={DesignPlan} />
-        <Stack.Screen name="MaterialsTab" component={MaterialsTab} />
         <Stack.Screen name="MainLanding" component={MainLanding} />
 
         <Stack.Screen name="DesignPlanViewer" component={DesignPlanViewer} />
